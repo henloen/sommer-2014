f4cf2f7ffe9fda4d7f3c3ee0456509e02e2c1105
larsesen <larsnedb@gmail.com>
2014-07-07 13:30:21 +0200

    working test paths with titleTest

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -5,28 +5,18 @@ exports.config = {
     'e2e/*.js'
   ],
 
- 
-// An example configuration file.
-exports.config = {
-  // The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-
-  // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
   },
 
-
-  baseUrl: 'http://localhost:3000',
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  //specs: ['mainpage.js'],
+  baseUrl: 'http://localhost:3000/public/',
 
   framework: 'jasmine',
 
-  // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000
   }
 };
+
+//npm run protractor
