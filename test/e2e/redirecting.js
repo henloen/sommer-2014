5192a729044ec75b1ffee076cfee563fe350c2dd
Henrik <henloen@gmail.com>
2014-07-11 14:00:00 +0200

    Added validation and changed some e2e tests to fill out answers so they pass

--- test/e2e/redirecting.js
+++ test/e2e/redirecting.js
@@ -36,6 +36,52 @@ describe('test redirection and Urls:', function() {
   	browser.findElement(by.name("startButton")).click();
    	expect(browser.getCurrentUrl()).toEqual("http://localhost:3000/public/#/partial-register-answer");
 
+     //fill out answers, needed to pass activate the button for registering the answer
+    var kjonn = browser.findElement(by.id("kjonn"));
+    kjonn.findElement(by.name("kvinne")).click();
+    
+
+    var sivilstatus = browser.findElement(by.id("sivilstatus"));
+    sivilstatus.findElement(by.name("singel")).click();
+    
+
+    var alder = browser.findElement(by.id("alder"));
+    alder.findElement(by.name("hipster")).click();
+    
+
+    var pahodet = browser.findElement(by.id("pa_hodet"));
+    pahodet.findElement(by.name("hette")).click();
+    
+
+    var studiested = browser.findElement(by.id("studiested"));
+    studiested.findElement(by.name("ntnu")).click();
+    
+
+    var programmeringsstil = browser.findElement(by.id("programmeringsstil"));
+    programmeringsstil.findElement(by.name("detordnerseg")).click();
+    
+
+    var musikk = browser.findElement(by.id("musikk"));
+    musikk.findElement(by.name("disco")).click();
+    
+
+    var personlighet = browser.findElement(by.id("personlighet"));
+    personlighet.findElement(by.name("ekstrovertpluss")).click();
+    
+
+    var hypepreferanse = browser.findElement(by.id("hypepreferanse"));
+    hypepreferanse.findElement(by.name("bigdata")).click();
+    
+
+    var planerforkvelden = browser.findElement(by.id("planerforkvelden"));
+    planerforkvelden.findElement(by.name("mingle")).click();
+    
+    var favorittgode = browser.findElement(by.id("favorittgode"));
+    favorittgode.findElement(by.name("fribar")).click();
+    
+    var premie = browser.findElement(by.id("premiehvisduvinner"));
+    premie.findElement(by.name("oculusrift")).click();
+
 
 	 browser.findElement(by.name("registerButton")).click();
    	expect(browser.getCurrentUrl()).toEqual("http://localhost:3000/public/#/partial-register-participant");   	
