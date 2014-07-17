3d942928ca3156614859ba23282ebdc3c1fd46cb
larsesen <larsnedb@gmail.com>
2014-07-17 16:17:50 +0200

    added new test and html tag

---
+++ test/e2e/denyDuplicateEmail.js
@@ -0,0 +1,38 @@
+var util = require("./utilities");
+
+describe('should test restrictions towards email database:', function() {
+  
+  var baseURL = "http://localhost:3000";
+  var timestamp = (new Date).getTime();
+
+  it('should allow entry of email', function() {    
+    util.directToIndex(browser);
+    util.clickRegisterAnswerButton(browser);
+    util.clickStartButton(browser);
+    util.fillAnswer(browser);
+    util.submitAnswers(browser);
+
+    util.registerParticipant(browser, timestamp);
+    util.submitParticipant(browser);
+
+    expect(browser.getCurrentUrl()).toEqual(baseURL + "/public/index.html#/partial-participant-registered");
+  });
+
+
+  it('should deny duplicate email entry', function() {
+
+    util.clickMenuButton(browser);
+    util.clickRegisterAnswerButton(browser);
+    util.clickStartButton(browser);
+    util.fillAnswer(browser);
+    util.submitAnswers(browser);
+
+    util.registerParticipant(browser, timestamp);
+    util.submitParticipant(browser)
+
+    //Won't be submitted because email already exists
+    expect(browser.getCurrentUrl()).toEqual(baseURL + "/public/index.html#/partial-register-participant");
+  });
+
+
+});
